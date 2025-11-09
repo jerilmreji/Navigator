a652fb835600daddad7722ffd7d30aba9bc77bd7
jerilmreji <jeril.reji14@gmail.com>
2025-11-09 21:13:02 +0530

    created a new listing and created a route for that

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/Navigator";
 
@@ -16,6 +17,20 @@ async function main() {
 
 app.get("/", (req, res) => {
     res.send("Hi, I'm root");
+});
+
+app.get("/testlisting", async (req, res) => {
+    let sampleListing = new Listing({
+        title: "My New Villa",
+        desciption: "By the valley",
+        price: 1200,
+        location: "punjab",
+        country: "India",
+    });
+
+    await sampleListing.save();
+    console.log("Sample was saved");
+    res.send("succesful testing");
 });
 
 app.listen(8080, () => {
---
+++ models/listing.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        type: String,
+        default: "https://unsplash.com/photos/trees-under-cloudy-sky-during-sunset--G3rw6Y02D0",
+        set : (v) => v === "" ? "https://unsplash.com/photos/trees-under-cloudy-sky-during-sunset--G3rw6Y02D0" : v,
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
