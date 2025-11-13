5ebc91da0dd76a17335e3fe35a06bd94402d0511
jerilmreji <jeril.reji14@gmail.com>
2025-11-13 21:41:57 +0530

    Initialized basic new route

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({extended: true}));
+
 const MONGO_URL = "mongodb://127.0.0.1:27017/Navigator";
 
 main().then(() => {
@@ -29,10 +31,23 @@ app.get("/listings", async (req, res) => {
     res.render("listings/index.ejs", {allListings});
 });
 
+//New Route
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs")
+});
+
+//Show Route
 app.get("/listings/:id", async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", {listing});
+});
+
+//Create Route
+app.post("/listings", async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
 });
 
 // app.get("/testlisting", async (req, res) => {
